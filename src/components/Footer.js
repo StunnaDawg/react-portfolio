796f2a099b73104fcc5e9795f8f0e383628aaaa4
StunnaDawg <jonsonallen9@gmail.com>
2023-06-06 20:21:02 -0300

    values in forms

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,10 @@ export default function Footer() {
     return (
     <footer class="footer">
         <h5>Made with ❤️</h5>
+        <ul class="contact-me-list">
+            <li><a href="https://github.com/StunnaDawg"> Github</a></li>
+            <li><a href="https://twitter.com/stunnacoding">Twitter</a></li>
+        </ul>
     </footer>
     )
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Contact() {
+    let [inputNameValue, setNameInputValue ] = useState('');
+    let [inputEmailValue, setEmailInputValue] = useState('')
+    let [inputMessageValue, setMessageInputValue] = useState('')
+
+    let handleNameChange = (event) => {
+        setNameInputValue(event.target.value);
+    }
+
+    let handleEmailChange = (event) => {
+        setEmailInputValue(event.target.value);
+    }
+
+    let handleMessageChange = (event) => {
+        setMessageInputValue(event.target.value);
+    }
+
     return (
-        <section class="contact-me" id="contact-form">
-        <h2>Contact Me!</h2>
-        <ul class="contact-me-list">
-            <li><a href="https://github.com/StunnaDawg"> Github</a></li>
-            <li><a href="https://twitter.com/stunnacoding">Twitter</a></li>
-            <li id="email-me"><a href="mailto: jonsonal@hotmail.com">Email</a></li>
-        </ul>
-    </section>
+        <div>
+    <h2>Contact Me!</h2>
+    <form>
+     <label>
+        Name:
+        <input 
+        type="text" value={inputNameValue} onChange={handleNameChange}
+        />
+        <p>{inputNameValue}</p>
+        </label>
+    </form>
+    <form>
+        <label>
+        email:
+        <input 
+        type="text" value={inputEmailValue} onChange={handleEmailChange}
+        />
+        <p>{inputEmailValue}</p>
+        </label>
+    </form>
+        <form>
+        message:
+        <input 
+        type="text" value={inputMessageValue} onChange={handleMessageChange}
+        />
+        <p>{inputMessageValue}</p>
+        <button onClick={ () => console.log('hey')} >Submit</button>
+        </form>
+
+        </div>
     )
 }

97c76b38b7cc41a664aea4977322b088b76f76f6
StunnaDawg <jonsonallen9@gmail.com>
2023-06-06 22:36:21 -0300

    setState email works

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,52 +1,92 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+
+function EmailSection() {
+  let [inputEmailValue, setEmailInputValue] = useState("");
+  let [emailStatus, setEmailStatus] = useState("wrong")
+
+  const regex = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+
+  let handleEmailChange = (event) => {
+    setEmailInputValue(event.target.value);
+    if (regex.test(event.target.value)) {
+      setEmailStatus("good")
+      console.log(emailStatus);
+    }
+  };
+  if (emailStatus === "wrong") {
+  return (
+    <form>
+      <label>
+        email:
+        <input
+          type="text"
+          value={inputEmailValue}
+          onChange={handleEmailChange}
+        />
+        <p>Not a valid Email!</p>
+      </label>
+    </form>
+  );
+  }
+
+  if (emailStatus === "good") {
+    return (
+      <form>
+        <label>
+          email:
+          <input
+            type="text"
+            value={inputEmailValue}
+            onChange={handleEmailChange}
+          />
+          <p>valid</p>
+        </label>
+      </form>
+    );
+    }
+}
 
 export default function Contact() {
-    let [inputNameValue, setNameInputValue ] = useState('');
-    let [inputEmailValue, setEmailInputValue] = useState('')
-    let [inputMessageValue, setMessageInputValue] = useState('')
+  let [inputNameValue, setNameInputValue] = useState("");
+  let [inputMessageValue, setMessageInputValue] = useState("");
 
-    let handleNameChange = (event) => {
-        setNameInputValue(event.target.value);
-    }
+  let handleNameChange = (event) => {
+    setNameInputValue(event.target.value);
+  };
 
-    let handleEmailChange = (event) => {
-        setEmailInputValue(event.target.value);
-    }
+  let handleMessageChange = (event) => {
+    setMessageInputValue(event.target.value);
+  };
 
-    let handleMessageChange = (event) => {
-        setMessageInputValue(event.target.value);
-    }
-
-    return (
-        <div>
-    <h2>Contact Me!</h2>
-    <form>
-     <label>
-        Name:
-        <input 
-        type="text" value={inputNameValue} onChange={handleNameChange}
-        />
-        <p>{inputNameValue}</p>
-        </label>
-    </form>
-    <form>
+  return (
+    <div>
+      <h2>Contact Me!</h2>
+      <form>
         <label>
-        email:
-        <input 
-        type="text" value={inputEmailValue} onChange={handleEmailChange}
-        />
-        <p>{inputEmailValue}</p>
+          Name:
+          <input
+            type="text"
+            value={inputNameValue}
+            onChange={handleNameChange}
+          />
+          <p>{inputNameValue}</p>
         </label>
-    </form>
-        <form>
+      </form>
+
+      <div>
+        <EmailSection />
+      </div>
+
+      <form>
         message:
-        <input 
-        type="text" value={inputMessageValue} onChange={handleMessageChange}
+        <input
+          type="text"
+          value={inputMessageValue}
+          onChange={handleMessageChange}
         />
         <p>{inputMessageValue}</p>
-        <button onClick={ () => console.log('hey')} >Submit</button>
-        </form>
-
-        </div>
-    )
+        <button onClick={() => console.log("hey")}>Submit</button>
+      </form>
+    </div>
+  );
 }
